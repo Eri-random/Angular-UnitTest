34284cca1bb1c646c3b1359f977c4b0aac65347f
Erika <brenherrera@alumno.unlam.edu.ar>
2024-05-13 10:27:26 -0300

    configuracion e instancia de test a cart component

---
+++ src/app/pages/cart/cart.component.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { CartComponent } from "./cart.component";
+import {HttpClientTestingModule} from "@angular/common/http/testing"
+import { BookService } from "src/app/services/book.service";
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+
+describe('Cart component',() =>{
+
+    let component: CartComponent;
+    let fixture : ComponentFixture<CartComponent>;
+
+    beforeEach(() =>{
+        TestBed.configureTestingModule({
+            imports:[
+            HttpClientTestingModule
+            ],
+            declarations:[
+            CartComponent
+            ],
+            providers:[
+                BookService
+            ],
+            schemas:[
+                CUSTOM_ELEMENTS_SCHEMA,
+                NO_ERRORS_SCHEMA
+            ]
+        });
+    });
+
+    //Instancia del componente
+    beforeEach(() =>{
+        fixture = TestBed.createComponent(CartComponent);
+        component = fixture.componentInstance;
+        //OnInit
+        fixture.detectChanges();
+    })
+});
