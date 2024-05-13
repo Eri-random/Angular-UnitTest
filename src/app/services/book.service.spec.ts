d18656935611ead3de82c978ee234a74faff3649
Erika <brenherrera@alumno.unlam.edu.ar>
2024-05-13 19:17:40 -0300

    test a servicio con set local storage

--- src/app/services/book.service.spec.ts
+++ src/app/services/book.service.spec.ts
@@ -4,6 +4,7 @@ import { TestBed } from "@angular/core/testing";
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
 import { Book } from "../models/book.model";
 import { environment } from "src/environments/environment.prod";
+import Swal from "sweetalert2";
 
 const listBook:Book[]=[
     {
@@ -29,9 +30,18 @@ const listBook:Book[]=[
       }
 ]
 
+    const book:Book = {
+        name:'',
+        author:'',
+        isbn:'',
+        price: 15,
+        amount:2
+    }
+
 describe('BookService',() =>{
     let service: BookService;
     let httpMock : HttpTestingController;
+    let storage : {};
 
     beforeEach(() =>{
         TestBed.configureTestingModule({
@@ -47,7 +57,15 @@ describe('BookService',() =>{
 
     beforeEach(()=>{
         service = TestBed.inject(BookService);
-        httpMock = TestBed.inject(HttpTestingController)
+        httpMock = TestBed.inject(HttpTestingController);
+        storage = {};
+        spyOn(localStorage, 'getItem').and.callFake ( (key: string) => {
+            return storage[key] ? storage[key] : null;
+        });
+        spyOn(localStorage, 'setItem').and.callFake ( (key: string, value:string) => {
+            return storage[key] = value;
+        });
+    
     });
 
     afterEach(() =>{
@@ -72,5 +90,51 @@ describe('BookService',() =>{
         expect(req.request.method).toBe('GET');
         //Simular el request
         req.flush(listBook);
+    });
+
+    // public getBooksFromCart(): Book[] {
+    //     let listBook: Book[] = JSON.parse(localStorage.getItem('listCartBook'));
+    //     if (listBook === null) {
+    //       listBook = [];
+    //     }
+    //     return listBook;
+    //   }
+
+    it('getBooksFromCart retrun empty array when localstorage is empty',() =>{
+        const listBook = service.getBooksFromCart();
+        expect(listBook.length).toBe(0);
+    });
+
+    // public addBookToCart(book: Book) {
+    //     let listBook: Book[] = JSON.parse(localStorage.getItem('listCartBook'));
+    //     if (listBook === null) { // Create a list with the book
+    //       book.amount = 1;
+    //       listBook = [ book ];
+    //     } else { 
+    //       const index = listBook.findIndex((item: Book) => {
+    //         return book.id === item.id;
+    //       });
+    //       if (index !== -1) { // Update the quantity in the existing book
+    //         listBook[index].amount++;
+    //       } else { 
+    //         book.amount = 1;
+    //         listBook.push(book);
+    //       }
+    //     }
+    //     localStorage.setItem('listCartBook', JSON.stringify(listBook));
+    //     this._toastSuccess(book);
+    //   }
+
+    it('addBookToCart when the list does not exist in the localstorage ',() =>{
+        const toast ={
+            fire:() => null
+        } as any;
+        const spy1 = spyOn(Swal,'mixin').and.callFake(() =>{
+            return toast;
+        })
+        service.addBookToCart(book);
+        const listBook = service.getBooksFromCart();
+        expect(listBook.length).toBe(1);
+        expect(spy1).toHaveBeenCalled();
     });
 })
