b7bffde38b66f7fe0d34b29b7c459b2abc9e4016
Erika <brenherrera@alumno.unlam.edu.ar>
2024-05-13 13:54:27 -0300

    mockear servicio en test

---
+++ src/app/pages/home/home.component.spec.ts
@@ -0,0 +1,87 @@
+import { BookService } from "src/app/services/book.service";
+import { HomeComponent } from "./home.component";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { Book } from "src/app/models/book.model";
+import { of } from "rxjs";
+
+const listBook:Book[]=[
+    {
+      name:'',
+      author:'',
+      isbn:'',
+      price: 15,
+      amount:2
+    },
+    {
+        name:'',
+        author:'',
+        isbn:'',
+        price: 20,
+        amount:1
+      },
+      {
+        name:'',
+        author:'',
+        isbn:'',
+        price: 8,
+        amount:7
+      }
+]
+
+const bookServiceMock={
+    getBooks : () => of(listBook),
+}
+
+describe('Home component',() =>{
+
+    let component: HomeComponent;
+    let fixture : ComponentFixture<HomeComponent>;
+    let service: BookService;
+
+    beforeEach(() =>{
+        TestBed.configureTestingModule({
+            imports:[
+            HttpClientTestingModule
+            ],
+            declarations:[
+            HomeComponent
+            ],
+            providers:[
+                //BookService
+                /*MOCKEAR SERVICE*/
+                {
+                    provide:BookService,
+                    useValue:bookServiceMock
+                }
+            ],
+            schemas:[
+                CUSTOM_ELEMENTS_SCHEMA,
+                NO_ERRORS_SCHEMA
+            ]
+        }).compileComponents();
+    });
+
+    //Instancia del componente
+    beforeEach(() =>{
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        //OnInit
+        fixture.detectChanges();
+        service = fixture.debugElement.injector.get(BookService);
+    });
+
+    //Comprobar que el componente se creo correctamentes
+    it('should create',() =>{
+        expect(component).toBeTruthy();
+    });
+
+    it('getBooks get books from the subscription', ()=>{
+        //const spy1 = spyOn(service,'getBooks').and.returnValue(of(listBook))
+        component.getBooks();
+        //expect(spy1).toHaveBeenCalled();
+        expect(component.listBook.length).toBe(3);
+    });
+
+});
