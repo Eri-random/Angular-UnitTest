dbe21be2998d994a29fea260e1d9976c75df2277
Erika <brenherrera@alumno.unlam.edu.ar>
2024-05-13 13:05:38 -0300

    test metodos sin return y con llamada a servicios y otro metodo

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -33,6 +33,7 @@ describe('Cart component',() =>{
 
     let component: CartComponent;
     let fixture : ComponentFixture<CartComponent>;
+    let service: BookService;
 
     beforeEach(() =>{
         TestBed.configureTestingModule({
@@ -58,6 +59,7 @@ describe('Cart component',() =>{
         component = fixture.componentInstance;
         //OnInit
         fixture.detectChanges();
+        service = fixture.debugElement.injector.get(BookService);
     });
 
     //Comprobar que el componente se creo correctamentes
@@ -71,4 +73,71 @@ describe('Cart component',() =>{
         expect(totalPrice).not.toBe(0);
         expect(totalPrice).not.toBeNull();
     })
+
+    
+//   public onInputNumberChange(action: string, book: Book): void {
+//     const amount = action === 'plus' ? book.amount + 1 : book.amount - 1;
+//     book.amount = Number(amount);
+//     this.listCartBook = this._bookService.updateAmountBook(book);
+//     this.totalPrice = this.getTotalPrice(this.listCartBook);
+//   }
+
+    it('onInputNumberChange increment correctly',()=>{
+        const action = 'plus';
+        const book =   {
+            name:'',
+            author:'',
+            isbn:'',
+            price: 15,
+            amount:2
+        };
+        
+        /*FORMAS DE INSTANCIAR SERVICIO*/
+        // const service = (component as any)._bookService;
+        // const service2 = component['_bookService'];
+
+        //const service = fixture.debugElement.injector.get(BookService);
+
+        //crear un espia y haz una llamada falsa al servicio
+        const spy1 = spyOn(service,'updateAmountBook').and.callFake(() => null);
+        const spy2 = spyOn(component,'getTotalPrice').and.callFake(() => null);
+
+        expect(book.amount).toBe(2);
+
+        component.onInputNumberChange(action,book);
+
+        expect(book.amount).toBe(3);
+        expect(book.amount === 3).toBeTrue();
+
+        expect(spy1).toHaveBeenCalled();
+        expect(spy2).toHaveBeenCalled();
+
+    });
+
+    
+    it('onInputNumberChange decrement correctly',()=>{
+        const action = 'minus';
+        const book = {
+            name:'',
+            author:'',
+            isbn:'',
+            price: 15,
+            amount:3
+        };
+        
+        const spy1 = spyOn(service,'updateAmountBook').and.callFake(() => null);
+        const spy2 = spyOn(component,'getTotalPrice').and.callFake(() => null);
+
+        expect(book.amount).toBe(3);
+
+        component.onInputNumberChange(action,book);
+
+        expect(book.amount).toBe(2);
+        expect(book.amount === 2).toBeTrue();
+
+        expect(spy1).toHaveBeenCalled();
+        expect(spy2).toHaveBeenCalled();
+
+    });
+
 });
