d16b51bb51c667ce752d4fc68e3458621906f1c0
Erika <brenherrera@alumno.unlam.edu.ar>
2024-05-13 11:16:05 -0300

    test con return. visualizar coverage

--- src/app/pages/cart/cart.component.spec.ts
+++ src/app/pages/cart/cart.component.spec.ts
@@ -3,6 +3,31 @@ import { CartComponent } from "./cart.component";
 import {HttpClientTestingModule} from "@angular/common/http/testing"
 import { BookService } from "src/app/services/book.service";
 import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from "@angular/core";
+import { Book } from "src/app/models/book.model";
+  
+const listBook:Book[]=[
+    {
+      name:'',
+      author:'',
+      isbn:'',
+      price: 15,
+      amount:2
+    },
+    {
+        name:'',
+        author:'',
+        isbn:'',
+        price: 20,
+        amount:1
+      },
+      {
+        name:'',
+        author:'',
+        isbn:'',
+        price: 8,
+        amount:7
+      }
+]
 
 describe('Cart component',() =>{
 
@@ -39,4 +64,11 @@ describe('Cart component',() =>{
     it('should create',() =>{
         expect(component).toBeTruthy();
     });
+
+    it('getTotalPrice returns an amount', ()=>{
+        const totalPrice = component.getTotalPrice(listBook);
+        expect(totalPrice).toBeGreaterThan(0);
+        expect(totalPrice).not.toBe(0);
+        expect(totalPrice).not.toBeNull();
+    })
 });
